0c96395b414c76f5322e91dd48db098b40afa493
giamat13 <197935266+giamat13@users.noreply.github.com>
2026-06-07 17:21:52 +0300

    Fix import statement for SmallFireball in SpicyRamenItem

--- src/main/java/com/food/giamat/item/SpicyRamenItem.java
+++ src/main/java/com/food/giamat/item/SpicyRamenItem.java
@@ -2,7 +2,7 @@ package com.food.giamat.item;
 
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.projectile.SmallFireball;
+import net.minecraft.world.entity.projectile.hurtingprojectile.SmallFireball;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
